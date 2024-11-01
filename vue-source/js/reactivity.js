0a0fdf05b1072525c3cfcc1b49402418c44bfcc9
chenamin <1010425372@qq.com>
2024-11-01 17:35:12 +0800

    vue设计与实现学习

---
+++ vue-source/js/reactivity.js
@@ -0,0 +1,96 @@
+function track(target, key) {
+  if (!activeEffect) return target[key];
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+
+  let deps = depsMap.get(key);
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()));
+  }
+  deps.add(activeEffect);
+  activeEffect.deps.push(deps);
+}
+function trigger(target, key) {
+  const depsMap = bucket.get(target);
+  if (!depsMap) return;
+  const effects = depsMap.get(key);
+  const effectsToRun = new Set();
+  effects &&
+    effects.forEach((effectFn) => {
+      if (effectFn !== activeEffect) {
+        effectsToRun.add(effectFn);
+      }
+    });
+  effectsToRun.forEach((effectFn) => {
+    if (effectFn.options.scheduler) {
+      effectFn.options.scheduler(effectFn);
+    } else {
+      effectFn();
+    }
+  });
+}
+
+function cleanup(effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i];
+    deps.delete(effectFn);
+  }
+  effectFn.deps.length = 0;
+}
+
+const bucket = new WeakMap();
+const data = { foo: 1 };
+
+const obj = new Proxy(data, {
+  get(target, key) {
+    track(target, key);
+    return target[key];
+  },
+  set(target, key, newVal) {
+    target[key] = newVal;
+    trigger(target, key);
+  },
+});
+
+let activeEffect;
+const effectStack = [];
+function effect(fn, options = {}) {
+  const effectFn = () => {
+    cleanup(effectFn);
+    activeEffect = effectFn;
+    effectStack.push(effectFn);
+    fn();
+    effectStack.pop();
+    activeEffect = effectStack[effectStack.length - 1];
+  };
+  effectFn.options = options;
+  effectFn.deps = [];
+  effectFn();
+}
+const jobQueue = new Set();
+const p = Promise.resolve();
+let isFlushing = false;
+function flushJob() {
+  if (isFlushing) return;
+  isFlushing = true;
+  p.then(() => {
+    jobQueue.forEach((job) => job());
+  }).finally(() => {
+    isFlushing = false;
+  });
+}
+effect(
+  () => {
+    console.log(obj.foo);
+  },
+  {
+    scheduler(fn) {
+      jobQueue.add(fn);
+      flushJob();
+    },
+  }
+);
+obj.foo++;
+obj.foo++;
---
+++ vue-source/js/render.js
@@ -0,0 +1,43 @@
+function mountElement(vnode, container) {
+  const el = document.createElement(vnode.tag);
+  for (const key in vnode.props) {
+    if (/^on/.test(key)) {
+      el.addEventListener(key.substr(2).toLowerCase(), vnode.props[key]);
+    }
+  }
+  if (typeof vnode.children === "string") {
+    el.appendChild(document.createTextNode(vnode.children));
+  } else if (Array.isArray(vnode.children)) {
+    vnode.children.forEach((child) => renderer(child, el));
+  }
+  container.appendChild(el);
+}
+function mountComponent(vnode, container) {
+  const subtree = vnode.tag.render();
+  renderer(subtree, container);
+}
+
+function renderer(vnode, container) {
+  if (typeof vnode.tag === "string") {
+    mountElement(vnode, container);
+  } else if (typeof vnode.tag === "object") {
+    mountComponent(vnode, container);
+  }
+}
+
+const MyComponent = {
+  render() {
+    return {
+      tag: "div",
+      props: {
+        onClick: () => alert("hello"),
+      },
+      children: "click me",
+    };
+  },
+};
+const vnode = {
+  tag: MyComponent,
+};
+
+renderer(vnode, document.body);
